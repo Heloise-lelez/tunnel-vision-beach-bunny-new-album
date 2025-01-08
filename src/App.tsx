dcbb110f6336c1740c6ab2799b9468e6ba49a92c
Héloïse Le Lez <lelez.heloise@gmail.com>
2025-01-08 13:05:39 +0100

    In progress - Title Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "destyle.css";
+import mrEscargot from "./assets/mrEscargot.svg";
+import mmeEscargot from "./assets/mmeEscargot.svg";
+import glitters from "./assets/glitters.svg";
+import ballons from "./assets/ballons.svg";
+
+import { useRive, Layout, Fit, Alignment } from "@rive-app/react-canvas";
+
+import "./App.css";
+
+export const RiveDemo = () => {
+  const { RiveComponent } = useRive({
+    src: "/animation/tunnelVision.riv", // Chemin absolu depuis public
+    stateMachines: "Motion",
+    layout: new Layout({
+      fit: Fit.FitWidth,
+      alignment: Alignment.Center,
+    }),
+    autoplay: true,
+  });
+
+  return <RiveComponent />;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <div className="stickers">
+        <div>
+          <img src={mmeEscargot} className="" alt="Un Escargot" />
+          <img src={glitters} className="" alt="Un Escargot" />
+        </div>
+        <div>
+          <img src={ballons} className="" alt="Un Escargot" />
+          <img src={mrEscargot} className="" alt="Un Escargot" />
+        </div>
+      </div>
+      <div className="newAlbum">New album soon!</div>
+      <div className="title">
+        <RiveDemo />
+      </div>
+      <div className="gap" />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className="scrollDownBtn">
+          Create your own pochette d'album!
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
